1669323fd0c594d11635f035581590e14cf175b4
birkin <birkin.diana@gmail.com>
2021-07-09 07:19:09 -0400

    loads api-key envar.

--- bib_redirector/src/lib.rs
+++ bib_redirector/src/lib.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::env::VarError;
+
+
 #[derive(Debug)]
 pub struct RedirectHelper {
     alma_api_url_template: String,
@@ -11,12 +15,27 @@ pub struct RedirectHelper {
 impl RedirectHelper {
 
     pub async fn new( bib: &str ) -> RedirectHelper {
+        // -- incorporate bib into url-template
         println!("bib in helper, ``{:?}``", bib);
         let alma_api_url_template = String::from("https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs?view=brief&expand=none&other_system_id=THE_BIB-01bu_inst&apikey=THE_API_KEY");
         println!("alma_api_url_template, ``{:?}``", alma_api_url_template);
         let url_with_bib = str::replace( &alma_api_url_template, "THE_BIB", bib );
         // let zz: () = url_with_bib;  // yields, found struct `std::string::String`
         println!("url_with_bib, ``{:?}``", url_with_bib);
+
+        // -- incorporate api-key into url-template
+        // let api_key = env::var( "BIB_REDIRECT_TEST__ALMA_API_KEY" );  // returns Result
+        let api_key: Result<String, VarError> = env::var("BIB_REDIRECT_TEST__ALMA_API_KEY");
+        match api_key {
+            Ok(_) => {},
+            Err(_err) => {
+                println!("api-key envar not found; quitting");
+                std::process::exit(-1);
+            }
+        };
+        let api_key: String = api_key.unwrap();  // this is ok because the error is handled above
+        println!("api_key, ``{:?}``", api_key);
+        // let zz: () = api_key;
 
         RedirectHelper { alma_api_url_template }
     }
