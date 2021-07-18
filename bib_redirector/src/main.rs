5c37c0ca51d7f84148a95f8e2c770058165000ec
birkin <birkin.diana@gmail.com>
2021-07-18 16:53:39 -0400

    updates return-val to handle two-possibilities.

--- bib_redirector/src/main.rs
+++ bib_redirector/src/main.rs
@@ -15,8 +15,32 @@ async fn root() -> &'static str {
 }
 
 
+
+// enum TypeOr<S, T> {
+//     Left(S),
+//     Right(T),
+// }
+
+
+// enum StrOrStrAndVec<'a> {
+//     Str(&'a str),
+//     StrAndVec(&'a str, Vec<usize>),
+// }
+
+// fn f3(flag: bool) -> StrOrStrAndVec<'static> {
+//     if flag {
+//         StrOrStrAndVec::StrAndVec("abc", vec![0, 1, 2])
+//     } else {
+//         StrOrStrAndVec::Str("abc")
+//     }
+// }
+
+
 #[get("/bib_redirect_tester/<bib>")]
-async fn rdrctr(bib: String) -> Redirect {
+// async fn rdrctr(bib: String) -> Redirect {
+// async fn rdrctr(bib: String) -> ResponseOrRedirect<'static> {
+// async fn rdrctr(bib: String) -> Result<&'static str, Redirect> {
+async fn rdrctr(bib: String) -> Result< Redirect, &'static str > {
 
     // -- setup
     println!( "perceived bibnum, ``{:?}``", bib);
@@ -24,32 +48,52 @@ async fn rdrctr(bib: String) -> Redirect {
     print!("alma-api-url, ``{:?}``", redirector.alma_api_url);
 
     // -- validate bib
-    // let ok: bool = redirector.validate_bib( &bib ).await;
-    // if ok != true {
-    //     panic!( "bad_bib" );
+    /*  see <https://github.com/SergioBenitez/Rocket/blob/693f4f9ee50057fc735e6e7037e6dee5b485ba10/examples/responders/src/main.rs#L79-L85>
+        ...or `Either` <https://github.com/SergioBenitez/Rocket/blob/693f4f9ee50057fc735e6e7037e6dee5b485ba10/examples/responders/src/main.rs#L124-L140>
+     */
+    let is_valid: bool = redirector.validate_bib( &bib ).await;
+    println!( "is_valid, ``{:?}``", is_valid );
+    if is_valid == false {
+        Err( "bad_bib" )
+    } else {
+        Ok( Redirect::temporary(uri!( "https://www.google.com/")) )
+    }
+
+
+    // #[get("/redir/<name>")]
+    // fn maybe_redir(name: &str) -> Result<&'static str, Redirect> {
+    //     match name {
+    //         "Sergio" => Ok("Hello, Sergio!"),
+    //         _ => Err(Redirect::to(uri!(redir_login))),
+    //     }
     // }
 
+
     // -- add check-digit
-    let updated_bib: String = redirector.add_check_digit( &bib ).await;
-    println!( "updated_bibnum, ``{:?}``", updated_bib );
+    // let updated_bib: String = redirector.add_check_digit( &bib ).await;
+    // println!( "updated_bibnum, ``{:?}``", updated_bib );
 
-    // -- build api-url
-    let url: String = redirector.build_api_url( &updated_bib ).await;
-    println!( "api-url, ``{:?}``", url );
+    // // -- build api-url
+    // let url: String = redirector.build_api_url( &updated_bib ).await;
+    // println!( "api-url, ``{:?}``", url );
 
-    // -- hit api
-    let mms_id: String = redirector.hit_alma_api( &url ).await;
-    println!( "mms_id, ``{:?}``", mms_id );
+    // // -- hit api
+    // let mms_id: String = redirector.hit_alma_api( &url ).await;
+    // println!( "mms_id, ``{:?}``", mms_id );
 
-    // -- build redirect url
-    let redirect_url_template: String = "https://brown.primo.exlibrisgroup.com/discovery/fulldisplay?docid=almaTHE_MMS_ID&context=L&vid=01BU_INST:BROWN".to_string();
-    let redirect_url: String = str::replace( &redirect_url_template, "THE_MMS_ID", &mms_id );
-    println!( "redirect_url, ``{:?}``", redirect_url );
+    // // -- build redirect url
+    // let redirect_url_template: String = "https://brown.primo.exlibrisgroup.com/discovery/fulldisplay?docid=almaTHE_MMS_ID&context=L&vid=01BU_INST:BROWN".to_string();
+    // let redirect_url: String = str::replace( &redirect_url_template, "THE_MMS_ID", &mms_id );
+    // println!( "redirect_url, ``{:?}``", redirect_url );
 
     // -- happy path redirect
     // Redirect::moved( uri!( "https://www.google.com/" ) )  // works
     // Redirect::temporary( uri!( "https://www.google.com/" ) )  // works
-    Redirect::temporary( redirect_url )
+
+    // Ok( Redirect::temporary( redirect_url ) )
+    // ResponseOrRedirect::Rdrct(Redirect::temporary( redirect_url ))
+
+    // Redirect::temporary( redirect_url )
 
 }
 
@@ -61,7 +105,9 @@ async fn info() -> &'static str {
 
     println!( "lib function call done" );
 
-    "coming: info-response"
+    let resp: &str = "coming: info-response";
+
+    resp
 
 }
 
